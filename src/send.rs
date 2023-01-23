6c6f79682cb8fb607c6ff152d978b72d13f1eebd
jacobkaufmann <jacobkaufmann18@gmail.com>
2023-01-22 17:10:30 -0700

    test: add unit tests for SendBuffer

--- src/send.rs
+++ src/send.rs
@@ -1,6 +1,7 @@
 use std::collections::VecDeque;
+use std::io;
 
-pub type Bytes = Vec<u8>;
+type Bytes = Vec<u8>;
 
 pub struct SendBuffer<const N: usize> {
     pending: VecDeque<Bytes>,
@@ -8,6 +9,7 @@ pub struct SendBuffer<const N: usize> {
 }
 
 impl<const N: usize> SendBuffer<N> {
+    /// Creates a new buffer.
     pub fn new() -> Self {
         Self {
             pending: VecDeque::new(),
@@ -15,11 +17,13 @@ impl<const N: usize> SendBuffer<N> {
         }
     }
 
+    /// Returns the number of bytes available in the buffer.
     pub fn available(&self) -> usize {
         N - self.pending.iter().fold(0, |acc, x| acc + x.len()) + self.offset
     }
 
-    pub fn write(&mut self, data: &[u8]) -> std::io::Result<usize> {
+    /// Writes `data` into the buffer, returning the number of bytes written.
+    pub fn write(&mut self, data: &[u8]) -> io::Result<usize> {
         let available = self.available();
         if data.len() <= available {
             self.pending.push_back(data.to_vec());
@@ -30,27 +34,122 @@ impl<const N: usize> SendBuffer<N> {
         }
     }
 
-    pub fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let mut written = 0;
-        let mut remaining = buf.len();
-        while remaining > 0 {
-            if let Some(data) = self.pending.front() {
-                let n = std::cmp::min(data.len() - self.offset, remaining);
-                buf[written..n].copy_from_slice(&data[self.offset..self.offset + n]);
+    /// Reads data from the buffer into `buf`, returning the number of bytes read.
+    ///
+    /// Data from at most one previous write can be read into `buf`. Data from different writes
+    /// will not go into a single read.
+    pub fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if buf.is_empty() {
+            return Ok(0);
+        }
 
-                written += n;
-                remaining -= n;
-                if self.offset + n == data.len() {
-                    self.offset = 0;
-                    self.pending.pop_front();
-                } else {
-                    self.offset += n;
-                }
+        let mut written = 0;
+        if let Some(data) = self.pending.front() {
+            let n = std::cmp::min(data.len() - self.offset, buf.len());
+            buf[..n].copy_from_slice(&data[self.offset..self.offset + n]);
+
+            written += n;
+            if self.offset + n == data.len() {
+                self.offset = 0;
+                self.pending.pop_front();
             } else {
-                break;
+                self.offset += n;
             }
         }
 
         Ok(written)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const SIZE: usize = 8192;
+
+    #[test]
+    fn available() {
+        let mut buf = SendBuffer::<SIZE>::new();
+        assert_eq!(buf.available(), SIZE);
+
+        const WRITE_LEN: usize = 512;
+        const NUM_WRITES: usize = 3;
+
+        const READ_LEN: usize = 64;
+        assert!(READ_LEN < WRITE_LEN);
+
+        for _ in 0..NUM_WRITES {
+            let data = vec![0; WRITE_LEN];
+            buf.write(&data).unwrap();
+        }
+        assert_eq!(buf.available(), SIZE - (WRITE_LEN * NUM_WRITES));
+
+        let mut data = vec![0; READ_LEN];
+        buf.read(&mut data).unwrap();
+        assert_eq!(buf.available(), SIZE - (WRITE_LEN * NUM_WRITES) + READ_LEN);
+
+        for _ in 0..NUM_WRITES {
+            let mut data = vec![0; WRITE_LEN];
+            buf.read(&mut data).unwrap();
+        }
+        assert_eq!(buf.available(), SIZE);
+    }
+
+    #[test]
+    fn read() {
+        let mut buf = SendBuffer::<SIZE>::new();
+
+        // Read of empty buffer returns zero.
+        let mut read_buf = vec![0; SIZE];
+        let read = buf.read(&mut read_buf).unwrap();
+        assert_eq!(read, 0);
+
+        const WRITE_LEN: usize = 1024;
+        assert!(WRITE_LEN < SIZE);
+
+        const READ_LEN: usize = 784;
+        assert!(READ_LEN < WRITE_LEN);
+        assert!(READ_LEN > WRITE_LEN / 2);
+
+        let mut read_buf = vec![0; READ_LEN];
+
+        let write_one = vec![0xef; WRITE_LEN];
+        let write_two = vec![0xfe; WRITE_LEN];
+        buf.write(&write_one).unwrap();
+        buf.write(&write_two).unwrap();
+
+        // Read first chunk of first write.
+        let read = buf.read(&mut read_buf).unwrap();
+        assert_eq!(read, READ_LEN);
+        assert_eq!(read_buf[..READ_LEN], write_one[..READ_LEN]);
+
+        // Read remaining chunk of first write.
+        let mut read_buf = vec![0; READ_LEN];
+        let read = buf.read(&mut read_buf).unwrap();
+        assert_eq!(read, WRITE_LEN - READ_LEN);
+        assert_eq!(read_buf[..WRITE_LEN - READ_LEN], write_one[READ_LEN..]);
+
+        // Read first chunk of second write.
+        let read = buf.read(&mut read_buf).unwrap();
+        assert_eq!(read, READ_LEN);
+        assert_eq!(read_buf[..READ_LEN], write_two[..READ_LEN]);
+
+        // Read with empty buffer returns zero.
+        let mut empty = vec![];
+        let read = buf.read(&mut empty).unwrap();
+        assert_eq!(read, 0);
+    }
+
+    #[test]
+    fn write() {
+        let mut buf = SendBuffer::<SIZE>::new();
+
+        const WRITE_LEN: usize = 1024;
+        assert!(WRITE_LEN < SIZE);
+
+        let data = vec![0xef; WRITE_LEN];
+        let written = buf.write(data.as_slice()).unwrap();
+        assert_eq!(written, WRITE_LEN);
+        assert_eq!(&buf.pending.pop_front().unwrap(), &data);
     }
 }
