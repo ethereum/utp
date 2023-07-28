7a3fb105744e23a4e96045d7557fbb45b7c2c27d
Jason Carver <ut96caarrs@snkmail.com>
2023-07-28 13:32:51 -0700

    Mock Socket & test that close() waits for ACK
    Use the new mock socket to:
    - remove chance of real dropped packets on UDP during test
    - allow parallel tests without fiddling with UDP ports
    - convenience method to launch connected peers
    - test what happens when network goes down
    
    Add test that close() waits for writes to ACK and eventually returns a
    timeout, by disabling the network after connecting to a peer. Closing
    the connection will time out, because the sender is waiting for ACKs
    that the recipient will never send. (Because the recipient never
    recieves the data)
    
    Why does this add the tokio test-util and start_paused = true?
    
    We don't want the test to actually wait the timeout length while
    running, so we use tokio's pause feature to skip over the timeout
    without interrupting anything else that would happen in real-time:
    https://docs.rs/tokio/latest/tokio/time/fn.pause.html

--- tests/stream.rs
+++ tests/stream.rs
@@ -1,47 +1,42 @@
-use std::net::SocketAddr;
+use std::io::ErrorKind;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use std::time::Duration;
 
-use utp_rs::cid;
+use tokio::time::timeout;
+
 use utp_rs::conn::ConnectionConfig;
 use utp_rs::socket::UtpSocket;
+
+use utp_rs::testutils;
 
 // Test that close() returns successful, after transfer is complete
 #[tokio::test]
 async fn close_is_successful_when_write_completes() {
     let conn_config = ConnectionConfig::default();
 
-    // TODO replace with a mock socket
-    let recv_addr = SocketAddr::from(([127, 0, 0, 1], 3406));
-    let recv = UtpSocket::bind(recv_addr).await.unwrap();
+    let ((send_link, send_cid), (recv_link, recv_cid)) = testutils::build_connected_pair();
+
+    let recv = UtpSocket::with_socket(recv_link);
     let recv = Arc::new(recv);
 
-    let send_addr = SocketAddr::from(([127, 0, 0, 1], 3407));
-    let send = UtpSocket::bind(send_addr).await.unwrap();
+    let send = UtpSocket::with_socket(send_link);
     let send = Arc::new(send);
-
-    let recv_one_cid = cid::ConnectionId {
-        send: 100,
-        recv: 101,
-        peer: send_addr,
-    };
-    let send_one_cid = cid::ConnectionId {
-        send: 101,
-        recv: 100,
-        peer: recv_addr,
-    };
 
     let recv_one = Arc::clone(&recv);
     let recv_one_handle = tokio::spawn(async move {
         recv_one
-            .accept_with_cid(recv_one_cid, conn_config)
+            .accept_with_cid(recv_cid, conn_config)
             .await
             .unwrap()
     });
 
+    // Keep a clone of the socket so that it doesn't drop when moved into the task.
+    // Dropping it causes all connections to exit.
     let send_one = Arc::clone(&send);
     let send_one_handle = tokio::spawn(async move {
         send_one
-            .connect_with_cid(send_one_cid, conn_config)
+            .connect_with_cid(send_cid, conn_config)
             .await
             .unwrap()
     });
@@ -74,13 +69,92 @@ async fn close_is_successful_when_write_completes() {
     let mut send_stream = send_stream_handle.await.unwrap();
 
     // close stream, which will wait for write to complete, and exit without a problem
-    send_stream.close().await.unwrap();
+    // This should happen extremely quickly.
+    match timeout(Duration::from_millis(20), send_stream.close()).await {
+        Ok(Ok(_)) => {}
+        Ok(Err(e)) => panic!("Error closing stream: {:?}", e),
+        Err(_) => panic!("Timeout closing stream"),
+    };
 
     // confirm that data is received as expected
     recv_stream_handle.await.unwrap();
 }
 
-// TODO after adding PerfectLink from stash@{1}: WIP on test-packet-drops: 7408259 Allow for mutable state in Socket send/recv
-// Test that close() returns a timeout, if recipient is not ACKing
-// AFter adding some mechanism that sends a RESET packet, add the following test:
-// Test that close() returns ConnectionReset if recipient resets during transfer
+// Test that close() returns a timeout, if recipient is not ACKing (after a successful connection)
+#[tokio::test(start_paused = true)]
+async fn close_errors_if_all_packets_dropped() {
+    let conn_config = ConnectionConfig::default();
+
+    let ((mut send_link, send_cid), (recv_link, recv_cid)) = testutils::build_connected_pair();
+    let tx_link_up = send_link.up_status();
+
+    let recv = UtpSocket::with_socket(recv_link);
+    let recv = Arc::new(recv);
+
+    let send = UtpSocket::with_socket(send_link);
+    let send = Arc::new(send);
+
+    let recv_one = Arc::clone(&recv);
+    let recv_one_handle = tokio::spawn(async move {
+        recv_one
+            .accept_with_cid(recv_cid, conn_config)
+            .await
+            .unwrap()
+    });
+
+    // Keep a clone of the socket so that it doesn't drop when moved into the task.
+    // Dropping it causes all connections to exit.
+    let send_one = Arc::clone(&send);
+    let send_one_handle = tokio::spawn(async move {
+        send_one
+            .connect_with_cid(send_cid, conn_config)
+            .await
+            .unwrap()
+    });
+
+    let (tx_one, rx_one) = tokio::join!(send_one_handle, recv_one_handle,);
+    let mut send_stream = tx_one.unwrap();
+    let mut recv_stream = rx_one.unwrap();
+
+    // ******* DISABLE NETWORK LINK ********
+    tx_link_up.store(false, Ordering::SeqCst);
+
+    // data to send
+    const DATA_LEN: usize = 100;
+    let data = [0xa5; DATA_LEN];
+
+    // send data
+    let send_stream_handle = tokio::spawn(async move {
+        match send_stream.write(&data).await {
+            Ok(written_len) => assert_eq!(written_len, DATA_LEN),
+            Err(e) => panic!("Error sending data: {:?}", e),
+        };
+        send_stream
+    });
+
+    // recv data
+    let recv_stream_handle = tokio::spawn(async move {
+        let mut read_buf = vec![];
+        let read_err = recv_stream.read_to_eof(&mut read_buf).await.unwrap_err();
+        assert_eq!(read_err.kind(), ErrorKind::TimedOut);
+    });
+
+    // Wait for send to start
+    let mut send_stream = send_stream_handle.await.unwrap();
+
+    // Close stream, which will fail because network is disabled.
+    match timeout(Duration::from_secs(20), send_stream.close()).await {
+        Ok(Ok(_)) => panic!("Stream closed successfully, but should have timed out"),
+        Ok(Err(e)) => {
+            // The stream must time out when waiting to close, if the network is disabled.
+            assert_eq!(e.kind(), ErrorKind::TimedOut);
+        }
+        Err(e) => panic!(
+            "The stream did not timeout on close() fast enough, giving up after: {:?}",
+            e
+        ),
+    };
+
+    // Wait to confirm that the read will time out, also.
+    recv_stream_handle.await.unwrap();
+}
