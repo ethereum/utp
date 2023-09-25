974c6d2b4bb8821b90de529f78388e5cbff5c7d1
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2023-09-24 20:37:46 -0600

    Fix ConnectionId hashmap key bug

--- src/cid.rs
+++ src/cid.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use std::fmt::Debug;
-use std::hash::Hash;
+use std::hash::{Hash, Hasher};
 use std::net::SocketAddr;
 
 /// A remote peer.
@@ -8,12 +8,27 @@ pub trait ConnectionPeer: Clone + Debug + Eq + Hash + PartialEq + Send + Sync {}
 
 impl ConnectionPeer for SocketAddr {}
 
-#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug)]
 pub struct ConnectionId<P> {
     pub send: u16,
     pub recv: u16,
     pub peer: P,
 }
+
+impl<P> Hash for ConnectionId<P> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.send.hash(state);
+        self.recv.hash(state);
+    }
+}
+
+impl<P> PartialEq for ConnectionId<P> {
+    fn eq(&self, other: &Self) -> bool {
+        self.send == other.send && self.recv == other.recv
+    }
+}
+
+impl<P> Eq for ConnectionId<P> {}
 
 pub trait ConnectionIdGenerator<P> {
     fn cid(&mut self, peer: P, is_initiator: bool) -> ConnectionId<P>;
